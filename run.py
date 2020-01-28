687ed83340d211e64a4e53995eb6670a18172673
jakebolduc <57921565+jakebolduc@users.noreply.github.com>
2020-01-27 20:50:13 -0500

    added output buffer, so output only occurs at the end of execution

--- run.py
+++ run.py
@@ -13,6 +13,7 @@ sensitivity = 0.8
 char_width_divided = round(char_width / 2)
 char_height_divided = round(char_height / 4)
 
+output_buffer = ""
 
 filename = input("file: ")
 
@@ -27,8 +28,8 @@ def image_average(x1, y1, x2, y2):
     return average([average(base.getpixel((x, y))[:3]) for x in range(x1, x2) for y in range(y1, y2)])
 def convert_index(x):
     return {3: 6, 4: 3, 5: 4, 6: 5}.get(x, x)
-for y in range(0, base.height - char_height - 1, char_height):
-    for x in range(0, base.width - char_width - 1, char_width):
+for y in range(0, base.height - char_height - 1, char_height): #line
+    for x in range(0, base.width - char_width - 1, char_width): #column
         byte = 0x0
         index = 0
         for xn in range(2):
@@ -37,5 +38,9 @@ for y in range(0, base.height - char_height - 1, char_height):
                 if match(avg + random.randint(-dither, dither), sensitivity * 0xFF):
                     byte += 2**convert_index(index)
                 index += 1
-        print(chr(start + byte), end = "")
-    print()
+        #print(chr(start + byte), end = "")
+        output_buffer = output_buffer + chr(start+byte)
+    #print()
+    output_buffer = output_buffer + "\n"
+    
+print(output_buffer)
